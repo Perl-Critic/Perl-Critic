7a17822982a65377168e3de1f53fe9aa97fe47b3
Andy Lester <andy@petdance.com>
2006-11-26 08:16:43 +0000

    Working thru t/20_policies_variables.t, and now need to make TODO tests work

---
+++ t/Variables/RequireInitializationForLocalVars.pl
@@ -0,0 +1,43 @@
+=name Basic
+
+=failures 6
+
+=cut
+
+local $foo;
+local ($foo, $bar);
+
+local $|;
+local ($|, $$);
+
+local $OUTPUT_RECORD_SEPARATOR;
+local ($OUTPUT_RECORD_SEPARATOR, $PROGRAM_NAME);
+
+#----------------------------------------------------------------
+
+=name Initialized passes
+
+=failures 0
+
+=cut
+
+local $foo = 'foo';
+local ($foo, $bar) = 'foo';       #Not right, but still passes
+local ($foo, $bar) = qw(foo bar);
+
+my $foo;
+my ($foo, $bar);
+our $bar
+our ($foo, $bar);
+
+#----------------------------------------------------------------
+
+=name key named "local"
+
+  local $TODO = "PPI bug prevents this from working";
+
+=failures 0
+
+=cut
+
+$x->{local};
