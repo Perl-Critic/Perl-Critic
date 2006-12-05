8e405d56f893ecdc6ee625dc5082fda11251c600
Elliot Shank <perl@galumph.com>
2006-12-05 03:46:14 +0000

    Work around a bug in PPI that doesn't return a location for a PPI::Statement for the code C<({})>.

--- lib/Perl/Critic/Policy/TestingAndDebugging/RequireUseStrict.pm
+++ lib/Perl/Critic/Policy/TestingAndDebugging/RequireUseStrict.pm
@@ -45,7 +45,10 @@ sub violates {
     for my $stmnt ( @{ $stmnts_ref } ) {
         last if $stmnt->isa('PPI::Statement::End');
         last if $stmnt->isa('PPI::Statement::Data');
-        my $stmnt_line = $stmnt->location()->[0];
+
+        # work around PPI bug: C<({})> results in a statement without a
+        # location.
+        my $stmnt_line = $stmnt->location() ? $stmnt->location()->[0] : -1;
         if ( (! defined $strict_line) || ($stmnt_line < $strict_line) ) {
             push @viols, $self->violation( $desc, $expl, $stmnt );
         }
