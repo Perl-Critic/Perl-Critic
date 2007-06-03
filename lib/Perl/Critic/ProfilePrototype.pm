bd3e7df1e04f5a2dbd5b2ee42b2c579511a069bb
Elliot Shank <perl@galumph.com>
2007-06-03 03:00:55 +0000

    ProfilePrototype now emits short Policy names instead of long ones.

--- lib/Perl/Critic/ProfilePrototype.pm
+++ lib/Perl/Critic/ProfilePrototype.pm
@@ -51,7 +51,7 @@ sub to_string {
 #
 sub _proto_format {
     return <<'END_OF_FORMAT';
-[%P]
+[%p]
 # set_themes = %t
 # severity   = %s
 %{\n%\x7b# \x7df\n# %n = %D\n}O%{# Cannot programmatically discover what parameters this policy takes.}U
