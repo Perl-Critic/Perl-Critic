b5705ab7b42d0eed898678d83eb85cff49385d64
Elliot Shank <perl@galumph.com>
2007-06-06 09:31:31 +0000

    Lower ProhibitLongChainsOfMethodCalls' default severity.

--- lib/Perl/Critic/Policy/ValuesAndExpressions/ProhibitLongChainsOfMethodCalls.pm
+++ lib/Perl/Critic/Policy/ValuesAndExpressions/ProhibitLongChainsOfMethodCalls.pm
@@ -36,7 +36,7 @@ sub supported_parameters {
     );
 }
 
-sub default_severity { return $SEVERITY_MEDIUM       }
+sub default_severity { return $SEVERITY_LOW          }
 sub default_themes   { return qw( core maintenance ) }
 sub applies_to       { return qw{ PPI::Statement };  }
 
