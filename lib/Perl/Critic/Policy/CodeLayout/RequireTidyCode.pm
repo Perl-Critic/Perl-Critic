032619333c93c369d00b6bd608e5ec4704ccb8c9
Elliot Shank <perl@galumph.com>
2008-06-17 18:28:56 +0000

    Change qr delimiters and insert some spaces to make regexes easier to read in RequireTidyCode.

--- lib/Perl/Critic/Policy/CodeLayout/RequireTidyCode.pm
+++ lib/Perl/Critic/Policy/CodeLayout/RequireTidyCode.pm
@@ -79,9 +79,9 @@ sub violates {
 
     # Remove the shell fix code from the top of program, if applicable
     ## no critic(ProhibitComplexRegexes)
-    my $shebang_re = qr{[#]![^\015\012]+[\015\012]+}xms;
-    my $shell_re   = qr{eval [ ] 'exec [ ] [^\015\012]* [ ] \$0 [ ] \${1[+]"\$@"}'
-                        [ \t]*[\012\015]+ [ \t]*if[^\015\012]+[\015\012]+}xms;
+    my $shebang_re = qr< [#]! [^\015\012]+ [\015\012]+ >xms;
+    my $shell_re   = qr<eval [ ] 'exec [ ] [^\015\012]* [ ] \$0 [ ] \${1[+]"\$@"}'
+                        [ \t]*[\012\015]+ [ \t]*if[^\015\012]+[\015\012]+ >xms;
     $source =~ s/\A ($shebang_re) $shell_re /$1/xms;
 
     my $dest    = $EMPTY;
