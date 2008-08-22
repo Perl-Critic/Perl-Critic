75a4e4c6a03eb9d647dc3b88f96a95be1926d3a8
Elliot Shank <perl@galumph.com>
2008-08-22 01:43:08 +0000

    Implement RegularExpressions::RequireDotMatchAnything.

---
+++ lib/Perl/Critic/Policy/RegularExpressions/RequireDotMatchAnything.pm
@@ -0,0 +1,115 @@
+##############################################################################
+#      $URL: http://perlcritic.tigris.org/svn/perlcritic/trunk/Perl-Critic/lib/Perl/Critic/Policy/RegularExpressions/RequireDotMatchAnything.pm $
+#     $Date: 2008-07-22 06:47:03 -0700 (Tue, 22 Jul 2008) $
+#   $Author: clonezone $
+# $Revision: 2609 $
+##############################################################################
+
+package Perl::Critic::Policy::RegularExpressions::RequireDotMatchAnything;
+
+use 5.006001;
+use strict;
+use warnings;
+
+use Readonly;
+
+use Perl::Critic::Utils qw{ :severities };
+use Perl::Critic::Utils::PPIRegexp qw{ &get_modifiers };
+
+use base 'Perl::Critic::Policy';
+
+our $VERSION = '1.090';
+
+#-----------------------------------------------------------------------------
+
+Readonly::Scalar my $DESC => q{Regular expression without "/s" flag};
+Readonly::Scalar my $EXPL => [ 240, 241 ];
+
+#-----------------------------------------------------------------------------
+
+sub supported_parameters { return ()                    }
+sub default_severity     { return $SEVERITY_LOW         }
+sub default_themes       { return qw<core pbp cosmetic> }
+sub applies_to           { return qw<PPI::Token::Regexp::Match
+                                     PPI::Token::Regexp::Substitute
+                                     PPI::Token::QuoteLike::Regexp> }
+
+#-----------------------------------------------------------------------------
+
+sub violates {
+    my ( $self, $elem, undef ) = @_;
+
+    my %modifiers = get_modifiers($elem);
+    if ( not $modifiers{s} ) {
+        return $self->violation( $DESC, $EXPL, $elem );
+    }
+    return; #ok!;
+}
+
+1;
+
+__END__
+
+#-----------------------------------------------------------------------------
+
+=pod
+
+=head1 NAME
+
+Perl::Critic::Policy::RegularExpressions::RequireDotMatchAnything - Always use the C</s> modifier with regular expressions.
+
+
+=head1 AFFILIATION
+
+This Policy is part of the core L<Perl::Critic|Perl::Critic>
+distribution.
+
+
+=head1 DESCRIPTION
+
+When asked what C<.> in a regular expression means, most people will
+say that it matches any character, which isn't true.  It's actually
+shorthand for C<[^\n]>.  Using the C<s> modifier makes C<.> act like
+people expect it to.
+
+    my $match = m< foo.bar >xm;  # not ok
+    my $match = m< foo.bar >xms; # ok
+
+
+=head1 CONFIGURATION
+
+This Policy is not configurable except for the standard options.
+
+
+=head1 NOTES
+
+Be cautions about slapping modifier flags onto existing regular
+expressions, as they can drastically alter their meaning.  See
+L<http://www.perlmonks.org/?node_id=484238> for an interesting
+discussion on the effects of blindly modifying regular expression
+flags.
+
+
+=head1 AUTHOR
+
+Jeffrey Ryan Thalhammer  <thaljef@cpan.org>
+
+
+=head1 COPYRIGHT
+
+Copyright (c) 2005-2008 Jeffrey Ryan Thalhammer. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.  The full text of this license
+can be found in the LICENSE file included with this module.
+
+=cut
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 78
+#   indent-tabs-mode: nil
+#   c-indentation-style: bsd
+# End:
+# ex: set ts=8 sts=4 sw=4 tw=78 ft=perl expandtab shiftround :
