4b1376656670e24991a48c8ef424d6f8f0e84d2d
Elliot Shank <perl@galumph.com>
2008-12-13 22:12:36 +0000

    Create P::C::Utils::Perl to hold symbol_without_sigil().

---
+++ lib/Perl/Critic/Utils/Perl.pm
@@ -0,0 +1,101 @@
+##############################################################################
+#      $URL$
+#     $Date$
+#   $Author$
+# $Revision$
+##############################################################################
+
+package Perl::Critic::Utils::Perl;
+
+use 5.006001;
+use strict;
+use warnings;
+
+use base 'Exporter';
+
+our $VERSION = '1.093_03';
+
+#-----------------------------------------------------------------------------
+
+our @EXPORT_OK = qw(
+    symbol_without_sigil
+);
+
+our %EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+
+#-----------------------------------------------------------------------------
+
+sub symbol_without_sigil {
+    my ($symbol) = @_;
+
+    (my $without_sigil = $symbol) =~ s< \A [\$@%*&] ><>xms;
+
+    return $without_sigil;
+}
+
+#-----------------------------------------------------------------------------
+
+1;
+
+__END__
+
+=pod
+
+=for stopwords
+
+=head1 NAME
+
+Perl::Critic::Utils::Perl - Utility functions for dealing with Perl language issues.
+
+
+=head1 SYNOPSIS
+
+    use Perl::Critic::Utils::Perl qw< :all >;
+
+    my $name = symbol_without_sigil('$foo');    # $name is "foo".
+
+
+=head1 DESCRIPTION
+
+This handles various issues with Perl, the language, that aren't necessarily
+L<PPI> related.
+
+
+=head1 IMPORTABLE SUBROUTINES
+
+=over
+
+=item C<symbol_without_sigil( $symbol )>
+
+Returns the name of the specified symbol with any sigil at the front.
+The parameter can be a vanilla Perl string or a L<PPI::Element>.
+
+
+=back
+
+
+=head1 AUTHOR
+
+Elliot Shank <perl@galumph.com>
+
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007-2008 Elliot Shank.  All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.  The full text of this license
+can be found in the LICENSE file included with this module.
+
+=cut
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 78
+#   indent-tabs-mode: nil
+#   c-indentation-style: bsd
+# End:
+# ex: set ts=8 sts=4 sw=4 tw=78 ft=perl expandtab shiftround :
---
+++ t/05_utils_perl.t
@@ -0,0 +1,60 @@
+#!perl
+
+##############################################################################
+#      $URL$
+#     $Date$
+#   $Author$
+# $Revision$
+##############################################################################
+
+use 5.006001;
+use strict;
+use warnings;
+
+use Readonly;
+
+use Test::More tests => 8;
+
+#-----------------------------------------------------------------------------
+
+our $VERSION = '1.093_03';
+
+#-----------------------------------------------------------------------------
+
+BEGIN {
+    use_ok('Perl::Critic::Utils::Perl', qw{ :all } );
+}
+
+#-----------------------------------------------------------------------------
+#  export tests
+
+can_ok('main', 'symbol_without_sigil');
+
+#-----------------------------------------------------------------------------
+#  name_without_sigil tests
+
+{
+    foreach my $sigil ( q<>, qw< $ @ % * & > ) {
+        my $symbol = "${sigil}foo";
+        is(
+            symbol_without_sigil($symbol),
+            'foo',
+            "symbol_without_sigil($symbol)",
+        );
+    }
+}
+
+#-----------------------------------------------------------------------------
+
+# ensure we run true if this test is loaded by
+# t/05_utils_ppi.t_without_optional_dependencies.t
+1;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 78
+#   indent-tabs-mode: nil
+#   c-indentation-style: bsd
+# End:
+# ex: set ts=8 sts=4 sw=4 tw=78 ft=perl expandtab shiftround :
