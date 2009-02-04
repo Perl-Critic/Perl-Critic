faca2eeb712a19e9fc042dcf66367f7251b74aba
Tom Wyant <harryfmudd@comcast.net>
2009-02-04 16:08:06 +0000

    RT #38678: ProtectPrivateSubs allow POSIX::_SC_CLK_TCK() and friends. Added 'accept' configuration item, pre-loaded with everything in \@POSIX::EXPORT that begins with an underscore.

--- lib/Perl/Critic/Policy/Subroutines/ProtectPrivateSubs.pm
+++ lib/Perl/Critic/Policy/Subroutines/ProtectPrivateSubs.pm
@@ -12,7 +12,7 @@ use strict;
 use warnings;
 use Readonly;
 
-use Perl::Critic::Utils qw{ :severities };
+use Perl::Critic::Utils qw{ :severities $EMPTY };
 use base 'Perl::Critic::Policy';
 
 our $VERSION = '1.096';
@@ -24,7 +24,60 @@ Readonly::Scalar my $EXPL => q{Use published APIs};
 
 #-----------------------------------------------------------------------------
 
-sub supported_parameters { return ()                     }
+sub supported_parameters {
+    return (
+        {
+            name            => 'allow',
+            description     =>
+                q<Subroutines beginning with '_' to allow under this policy.>,
+            default_string  => $EMPTY,
+            behavior        => 'string list',
+            list_always_present_values => [ qw<
+                POSIX::_PC_CHOWN_RESTRICTED
+                POSIX::_PC_LINK_MAX
+                POSIX::_PC_MAX_CANON
+                POSIX::_PC_MAX_INPUT
+                POSIX::_PC_NAME_MAX
+                POSIX::_PC_NO_TRUNC
+                POSIX::_PC_PATH_MAX
+                POSIX::_PC_PIPE_BUF
+                POSIX::_PC_VDISABLE
+                POSIX::_POSIX_ARG_MAX
+                POSIX::_POSIX_CHILD_MAX
+                POSIX::_POSIX_CHOWN_RESTRICTED
+                POSIX::_POSIX_JOB_CONTROL
+                POSIX::_POSIX_LINK_MAX
+                POSIX::_POSIX_MAX_CANON
+                POSIX::_POSIX_MAX_INPUT
+                POSIX::_POSIX_NAME_MAX
+                POSIX::_POSIX_NGROUPS_MAX
+                POSIX::_POSIX_NO_TRUNC
+                POSIX::_POSIX_OPEN_MAX
+                POSIX::_POSIX_PATH_MAX
+                POSIX::_POSIX_PIPE_BUF
+                POSIX::_POSIX_SAVED_IDS
+                POSIX::_POSIX_SSIZE_MAX
+                POSIX::_POSIX_STREAM_MAX
+                POSIX::_POSIX_TZNAME_MAX
+                POSIX::_POSIX_VDISABLE
+                POSIX::_POSIX_VERSION
+                POSIX::_SC_ARG_MAX
+                POSIX::_SC_CHILD_MAX
+                POSIX::_SC_CLK_TCK
+                POSIX::_SC_JOB_CONTROL
+                POSIX::_SC_NGROUPS_MAX
+                POSIX::_SC_OPEN_MAX
+                POSIX::_SC_PAGESIZE
+                POSIX::_SC_SAVED_IDS
+                POSIX::_SC_STREAM_MAX
+                POSIX::_SC_TZNAME_MAX
+                POSIX::_SC_VERSION
+                POSIX::_exit
+            > ],
+        },
+    );
+}
+
 sub default_severity     { return $SEVERITY_MEDIUM       }
 sub default_themes       { return qw( core maintenance ) }
 sub applies_to           { return 'PPI::Token::Word'     }
@@ -53,7 +106,8 @@ sub violates {
 sub _is_other_pkg_private_function {
     my ( $self, $elem ) = @_;
     return $elem =~ m{ (\w+)::_\w+ \z }xms
-        && $elem !~ m{ \A SUPER::_\w+ \z }xms;
+        && $elem !~ m{ \A SUPER::_\w+ \z }xms
+        && !$self->{_allow}{$elem};
 }
 
 sub _is_other_pkg_private_method {
@@ -71,6 +125,9 @@ sub _is_other_pkg_private_method {
     # shift->_private_method();  This is typically used as
     # shorthand for "my $self=shift; $self->_private_method()"
     return if $pkg eq 'shift' or $pkg eq '__PACKAGE__';
+
+    # Maybe the user wanted to exempt this explicitly.
+    return if $self->{_allow}{"${pkg}::$elem"};
 
     return 1;
 }
@@ -100,11 +157,22 @@ indicate private methods and variables by inserting a leading
 underscore before the identifier.  This policy catches attempts to
 access private variables from outside the package itself.
 
+The subroutines in the POSIX package which begin with an underscore
+(e.g. POSIX::_POSIX_ARG_MAX) are not flagged as errors by this policy.
+
 
 =head1 CONFIGURATION
 
-This Policy is not configurable except for the standard options.
+The user can configure additional subroutines to accept by specifying
+them in a space-delimited list to the C<accept> option:
 
+    [Subroutines::ProtectPrivateSubs]
+    allow = FOO::_bar FOO::_baz
+
+These are added to the default list of exemptions from this policy.
+Allowing a subroutine also allows the corresponding method call. So
+C<< FOO::_bar >> in the above example allows both C<< FOO::_bar() >> and
+C<< FOO->_bar() >>.
 
 =head1 HISTORY
 
