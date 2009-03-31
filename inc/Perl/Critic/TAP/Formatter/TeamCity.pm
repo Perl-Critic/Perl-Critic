a77ea17dce7772605a2676691c0d20cc060a374d
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2009-03-31 06:40:23 +0000

    Just whitespace changes

--- inc/Perl/Critic/TAP/Formatter/TeamCity.pm
+++ inc/Perl/Critic/TAP/Formatter/TeamCity.pm
@@ -24,49 +24,49 @@ sub summary {}
 #-----------------------------------------------------------------------------
 
 sub open_test {
-  my ($self, $test, $parser) = @_;
+    my ($self, $test, $parser) = @_;
 
-  teamcity_emit_build_message('testSuiteStarted', name => $test);
+    teamcity_emit_build_message('testSuiteStarted', name => $test);
 
-  my $session = $self->SUPER::open_test($test, $parser);
+    my $session = $self->SUPER::open_test($test, $parser);
 
-  while ( defined( my $result = $parser->next() ) ) {
-    next if not $result->is_test();
-    $self->_emit_teamcity_build_messages($result);
-    exit 1 if $result->is_bailout;
-  }
+    while ( defined( my $result = $parser->next() ) ) {
+        next if not $result->is_test();
+        $self->_emit_teamcity_build_messages($result);
+        exit 1 if $result->is_bailout;
+    }
 
-  teamcity_emit_build_message('testSuiteFinished', name => $test);
+    teamcity_emit_build_message('testSuiteFinished', name => $test);
 
-  return $session;
+    return $session;
 }
 
 #-----------------------------------------------------------------------------
 
 sub _emit_teamcity_build_messages {
-  my ($self, $result) = @_;
+    my ($self, $result) = @_;
 
-  my $test_name = $result->description() || 'No test name given';
-  my $expl = $result->explanation() || 'No explanation given';
+    my $test_name = $result->description() || 'No test name given';
+    my $expl = $result->explanation() || 'No explanation given';
 
-  teamcity_emit_build_message('testStarted', name => $test_name);
+    teamcity_emit_build_message('testStarted', name => $test_name);
 
-  if ( $result->has_todo() ) {
-    teamcity_emit_build_message('testIgnored', name => $test_name,  message => $expl);
-  }
-  elsif ( $result->has_skip() ) {
-    teamcity_emit_build_message('testIgnored', name => $test_name,  message => $expl);
-  }
-  elsif ( $result->is_unknown() ) {
-    teamcity_emit_build_message('testFailed', name => $test_name,  message => $expl);
-  }
-  elsif ( not $result->is_ok() ) {
-    teamcity_emit_build_message('testFailed', name => $test_name,  message => $expl);
-  }
+    if ( $result->has_todo() ) {
+        teamcity_emit_build_message('testIgnored', name => $test_name,  message => $expl);
+    }
+    elsif ( $result->has_skip() ) {
+        teamcity_emit_build_message('testIgnored', name => $test_name,  message => $expl);
+    }
+    elsif ( $result->is_unknown() ) {
+        teamcity_emit_build_message('testFailed', name => $test_name,  message => $expl);
+    }
+    elsif ( not $result->is_ok() ) {
+        teamcity_emit_build_message('testFailed', name => $test_name,  message => $expl);
+    }
 
-  teamcity_emit_build_message('testFinsihed', name => $test_name);
+    teamcity_emit_build_message('testFinsihed', name => $test_name);
 
-  return;
+    return;
 }
 
 #-----------------------------------------------------------------------------
