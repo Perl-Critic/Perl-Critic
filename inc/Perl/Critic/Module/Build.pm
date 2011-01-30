91c348c8d2217ce25f1bd73b538707cdfc1378ea
Elliot Shank <perl@galumph.com>
2011-01-30 22:47:52 +0000

    Make P::C::Module::Build a subclass of P::C::Module::Build::Standard.

--- inc/Perl/Critic/Module/Build.pm
+++ inc/Perl/Critic/Module/Build.pm
@@ -18,27 +18,7 @@ use Carp;
 use English qw< $OS_ERROR $EXECUTABLE_NAME -no_match_vars >;
 
 
-use base 'Module::Build';
-
-
-sub ACTION_authortest {
-    my ($self) = @_;
-
-    $self->_authortest_dependencies();
-    $self->depends_on('test');
-
-    return;
-}
-
-
-sub ACTION_authortestcover {
-    my ($self) = @_;
-
-    $self->_authortest_dependencies();
-    $self->depends_on('testcover');
-
-    return;
-}
+use base 'Perl::Critic::Module::Build::Standard';
 
 
 sub ACTION_policysummary {
@@ -56,15 +36,6 @@ sub ACTION_policysummary {
 }
 
 
-sub ACTION_distdir {
-    my ($self, @arguments) = @_;
-
-    $self->depends_on('authortest');
-
-    return $self->SUPER::ACTION_distdir(@arguments);
-}
-
-
 sub ACTION_nytprof {
     my ($self) = @_;
 
@@ -75,41 +46,11 @@ sub ACTION_nytprof {
 }
 
 
-sub ACTION_manifest {
-    my ($self, @arguments) = @_;
-
-    # Make sure we get rid of files that no longer exist.
-    if (-e 'MANIFEST') {
-        unlink 'MANIFEST' or die "Can't unlink MANIFEST: $OS_ERROR";
-    }
-
-    return $self->SUPER::ACTION_manifest(@arguments);
-}
-
-
-sub tap_harness_args {
+sub authortest_dependencies {
     my ($self) = @_;
 
-    return $self->_tap_harness_args() if $ENV{RUNNING_UNDER_TEAMCITY};
-    return;
-}
-
-
-sub _tap_harness_args {
-    return {formatter_class => 'TAP::Formatter::TeamCity', merge => 1};
-}
-
-
-sub _authortest_dependencies {
-    my ($self) = @_;
-
-    $self->depends_on('build');
     $self->depends_on('policysummary');
-    $self->depends_on('manifest');
-    $self->depends_on('distmeta');
-
-    $self->test_files( qw< t xt/author > );
-    $self->recursive_test_files(1);
+    $self->SUPER::authortest_dependencies();
 
     return;
 }
@@ -170,46 +111,15 @@ Perl::Critic::Module::Build - Customization of L<Module::Build> for L<Perl::Crit
 
 =head1 DESCRIPTION
 
-This is a custom subclass of L<Module::Build> that enhances existing
-functionality and adds more for the benefit of installing and
-developing L<Perl::Critic>.  The following actions have been added
-or redefined:
+This is a custom subclass of L<Module::Build> (actually,
+L<Perl::Critic::Module::Build::Standard>) that enhances existing functionality
+and adds more for the benefit of installing and developing L<Perl::Critic>.
+The following actions have been added or redefined:
 
 
 =head1 ACTIONS
 
 =over
-
-=item test
-
-In addition to the standard action, this adds a dependency upon the
-C<manifest> action.
-
-
-=item authortest
-
-Runs the regular tests plus the author tests (those in F<xt/author>).
-It used to be the case that author tests were run if an environment
-variable was set or if a F<.svn> directory existed.  What ended up
-happening was that people that had that environment variable set for
-other purposes or who had done a checkout of the code repository would
-run those tests, which would fail, and we'd get bug reports for
-something not expected to run elsewhere.  Now, you've got to
-explicitly ask for the author tests to be run.
-
-
-=item authortestcover
-
-As C<authortest> is to the standard C<test> action, C<authortestcover>
-is to the standard C<testcover> action.
-
-
-=item distdir
-
-In addition to the standard action, this adds a dependency upon the
-C<authortest> action so you can't do a release without passing the
-author tests.
-
 
 =item policysummary
 
