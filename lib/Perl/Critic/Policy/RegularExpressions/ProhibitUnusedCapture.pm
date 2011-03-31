f0976931bc2b272207049a6220e1ff17a0ecd17f
Tom Wyant <harryfmudd@comcast.net>
2011-03-31 15:31:33 +0000

    RT #67116 - Crash in RegularExpressions::ProhibitUnusedCapture
    This turned out to be a bug in the work that taught the policy to look
    inside here documents -- the code assumed that the PPI::Token::HereDoc
    heredoc() method returned the here document as a string, when it
    actually returned an array. The self-test only passed because it used
    one-line here documents.
    
    Mea culpa, mea maxima culpa.
    
    I'm afraid this is going to need another release. My apologies.

--- lib/Perl/Critic/Policy/RegularExpressions/ProhibitUnusedCapture.pm
+++ lib/Perl/Critic/Policy/RegularExpressions/ProhibitUnusedCapture.pm
@@ -19,7 +19,8 @@ use Scalar::Util qw(refaddr);
 
 use Perl::Critic::Exception::Fatal::Internal qw{ throw_internal };
 use Perl::Critic::Utils qw{
-    :booleans :severities hashify precedence_of split_nodes_on_comma
+    :booleans :characters :severities hashify precedence_of
+    split_nodes_on_comma
 };
 use base 'Perl::Critic::Policy';
 
@@ -515,7 +516,8 @@ sub _mark_magic {
     if ( $elem->isa( 'PPI::Token::HereDoc' ) ) {
         $elem->content() =~ m/ \A << \s* ' /sxm
             or _mark_magic_in_content(
-            $elem->heredoc(), $re, $captures, $named_captures, $doc );
+            join( $EMPTY, $elem->heredoc() ), $re, $captures,
+            $named_captures, $doc );
         return;
     }
 
