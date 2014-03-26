bef6c92eea6b380683f0bff65a8fb9d786faed4f
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2014-03-25 23:56:31 -0700

    Remove workaround for PPI parsing bug on &&= and ||=
    As of version 1.216_01, PPI correctly parses these as
    one operator instead of two.  So we need to remove our
    workaround and require the newer version of PPI.
    
    I feel icky about listing a dev version as a prereq.
    We should update that and cut a new version of P::C
    as soon as PPI makes a production release.

--- lib/Perl/Critic/Policy/ValuesAndExpressions/RequireConstantVersion.pm
+++ lib/Perl/Critic/Policy/ValuesAndExpressions/RequireConstantVersion.pm
@@ -117,25 +117,15 @@ sub violates {
 
 #-----------------------------------------------------------------------------
 
-# Check for an assignment operator. This is made more complicated by the fact
-# that PPI parses things like '||=' as two PPI::Token::Operators: '||' and
-# '='. So we take the first presumptive operator as an argument. If it is not
-# a PPI::Token::Operator, we return. If it's '=', we return it. If it is any
-# other operator, we see if the next significant token is '=', and if so
-# return that.
+# Check if the element is an assignment operator. 
 
 sub _check_for_assignment_operator {
     my ( $operator ) = @_;
 
-    return           if not $operator->isa( 'PPI::Token::Operator' );
-    return $operator if $EQUAL eq $operator->content();
+    return if not $operator->isa( 'PPI::Token::Operator' );
 
-    my $next;
-    return       if not $next = $operator->snext_sibling();
-    return       if not $next->isa( 'PPI::Token::Operator' );
-    return $next if $EQUAL eq $next->content();
-
-    return;
+    # This should match = ||= &&= += etc. but not =~
+    return $operator->content() =~ m/$EQUAL \Z/xms ? $operator : ();
 }
 
 #-----------------------------------------------------------------------------
