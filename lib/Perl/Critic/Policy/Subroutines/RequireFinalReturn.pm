b9c4c11fe2f86c51988d1f684b9118161ae6fba2
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2014-11-16 01:39:04 -0800

    Add the yada-yada operator as a terminal function (Fixes #574).
    The new ... operator is essentially the same as die().

--- lib/Perl/Critic/Policy/Subroutines/RequireFinalReturn.pm
+++ lib/Perl/Critic/Policy/Subroutines/RequireFinalReturn.pm
@@ -27,7 +27,7 @@ sub supported_parameters {
             default_string  => $EMPTY,
             behavior        => 'string list',
             list_always_present_values =>
-                [ qw< croak confess die exec exit throw Carp::confess Carp::croak > ],
+                [ qw< croak confess die exec exit throw Carp::confess Carp::croak ...> ],
         },
     );
 }
