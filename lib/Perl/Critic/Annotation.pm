eeae9f758748832b35ef0087dd8ffba7871ee9c0
Andy Lester <andy@petdance.com>
2017-11-21 16:07:42 -0600

    Fixed a typo.

--- lib/Perl/Critic/Annotation.pm
+++ lib/Perl/Critic/Annotation.pm
@@ -292,7 +292,7 @@ Perl::Critic::Annotation - A "## no critic" annotation in a document.
 =head1 DESCRIPTION
 
 C<Perl::Critic::Annotation> represents a single C<"## no critic">
-annotation in a L<PPI:Document>.  The Annotation takes care of parsing
+annotation in a L<PPI::Document>.  The Annotation takes care of parsing
 the annotation and keeps track of which lines and Policies it affects.
 It is intended to encapsulate the details of the no-critic
 annotations, and to provide a way for Policy objects to interact with
