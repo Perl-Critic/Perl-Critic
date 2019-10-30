99aab5f8a21354e28ba8907e0fb947002d201482
Andy Lester <andy@petdance.com>
2019-10-30 09:37:19 -0500

    Removed inaccurate "All rights reserved"

--- lib/Test/Perl/Critic/Policy.pm
+++ lib/Test/Perl/Critic/Policy.pm
@@ -414,7 +414,7 @@ Andy Lester, Jeffrey Ryan Thalhammer <thaljef@cpan.org>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2009-2011 Andy Lester.  All rights reserved.
+Copyright (c) 2009-2011 Andy Lester
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.  The full text of this license
