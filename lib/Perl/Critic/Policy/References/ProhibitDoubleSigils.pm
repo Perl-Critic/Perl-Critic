997a465f96b0ce7f6c68ad3f6ef05342e1cd4c5d
Ilya Rassadin <elcamlost@gmail.com>
2019-12-04 10:57:00 +0300

    allow postfix dereferencing with ProhibitDoubleSigils Policy

--- lib/Perl/Critic/Policy/References/ProhibitDoubleSigils.pm
+++ lib/Perl/Critic/Policy/References/ProhibitDoubleSigils.pm
@@ -27,10 +27,12 @@ sub applies_to           { return 'PPI::Token::Cast'    }
 sub violates {
     my ( $self, $elem, undef ) = @_;
     return if $elem eq q{\\};
+    return if $elem =~ /[@ $ % * &] [*]/xms;
+    return if $elem eq q{$#*}; ## no critic (RequireInterpolationOfMetachars)
 
     my $sib = $elem->snext_sibling;
     return if !$sib;
-    if ( ! $sib->isa('PPI::Structure::Block') ) {
+    if ( ! $sib->isa('PPI::Structure::Block') && ! $sib->isa('PPI::Structure::Subscript') ) {
         return $self->violation( $DESC, $EXPL, $elem );
     }
     return; #ok!
