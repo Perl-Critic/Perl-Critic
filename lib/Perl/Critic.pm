8faa663a297a6169b1b46306da248aceba63e5fc
Andy Lester <andy@petdance.com>
2022-12-03 23:34:19 -0600

    Added more thanks

--- lib/Perl/Critic.pm
+++ lib/Perl/Critic.pm
@@ -948,6 +948,8 @@ L<http://www.perlfoundation.org/april_1_2007_new_grant_awards>
 
 Thanks also to this incomplete laundry list of folks who have contributed
 to Perl::Critic in some way:
+Amory Meltzer,
+Andrew Grechkin,
 Daniel Mita,
 Gregory Oschwald,
 Mike O'Regan,
