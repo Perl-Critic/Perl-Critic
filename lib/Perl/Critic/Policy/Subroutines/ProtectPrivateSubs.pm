78e0948a5aa6dc476a50d3ca245940f958120c6d
Andy Lester <andy@petdance.com>
2022-12-11 00:05:35 -0600

    Minimize calls to ->content

--- lib/Perl/Critic/Policy/Subroutines/ProtectPrivateSubs.pm
+++ lib/Perl/Critic/Policy/Subroutines/ProtectPrivateSubs.pm
@@ -9,7 +9,7 @@ use English qw< $EVAL_ERROR -no_match_vars >;
 use Readonly;
 
 use Perl::Critic::Utils qw<
-    :severities $EMPTY is_function_call is_method_call
+    :severities $EMPTY is_function_call is_method_call hashify
 >;
 use parent 'Perl::Critic::Policy';
 
@@ -157,11 +157,11 @@ sub _is_other_pkg_private_method {
     my $package = $operator->sprevious_sibling() or return;
     return if not $package->isa('PPI::Token::Word');
 
-    # sometimes the previous sib is a keyword, as in:
+    # Sometimes the previous sib is a keyword, as in:
     # shift->_private_method();  This is typically used as
     # shorthand for "my $self=shift; $self->_private_method()"
-    return if $package->content() eq 'shift'
-        or $package->content() eq '__PACKAGE__';
+    state $keywords = { hashify( qw( shift __PACKAGE__ ) ) };
+    return if $keywords{$package->content()};
 
     # Maybe the user wanted to exempt this explicitly.
     return if $self->{_allow}{"${package}::$content"};
