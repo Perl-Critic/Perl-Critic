134121cd8de12cf0a10456d06d7f6166c0b902c5
Paul Johnson <paul@pjcj.net>
2024-01-12 11:37:07 +0100

    Better count signature arguments
    Signatures are returned as prototypes.  For most simple cases the
    prototype counting works for signatures as well, but the major problem
    is when the signature arguments contain underscores which causes them to
    be counted more than once.
    
    We can mostly distinguish between prototypes and signatures by checking
    for the presence of letters.  The perlsub documentation states "A valid
    prototype cannot contain any alphabetic character."
    
    This should be good enough to solve the most important problem without
    causing too many other difficulties.  Potential problematic areas
    include invalid prototypes and signatures only including non-alphabetic
    characters:
    
      perl -Mutf8 -E 'sub n ($𐠒) { say $𐠒 } n 42'
    
    Perhaps you could argue that here people will get what they deserve.

--- lib/Perl/Critic/Policy/Subroutines/ProhibitManyArgs.pm
+++ lib/Perl/Critic/Policy/Subroutines/ProhibitManyArgs.pm
@@ -55,8 +55,12 @@ sub violates {
     my $num_args;
     if ($elem->prototype) {
         my $prototype = $elem->prototype();
-        $prototype =~ s/ \\ [[] .*? []] /*/smxg;    # Allow for grouping
-        $num_args = $prototype =~ tr/$@%&*_+/$@%&*_+/;    # RT 56627
+        if ($prototype =~ /[a-z]/i) {  # signature (probably)
+            $num_args = $prototype =~ tr/$@%/$@%/;
+        } else {  # prototype
+            $prototype =~ s/ \\ [[] .*? []] /*/smxg;    # Allow for grouping
+            $num_args = $prototype =~ tr/$@%&*_+/$@%&*_+/;    # RT 56627
+        }
     } else {
         $num_args = _count_args($self->{_skip_object}, $elem->block->schildren);
     }
